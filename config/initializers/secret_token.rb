69064106a4cdee2b9da8f37cf650a836b5e07578
asin <mein@gmail.com>
2013-09-10 12:50:32 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsinApp::Application.config.secret_key_base = 'cceed172fb22d37fdf5d4c9d12a6b0539fe714c8f51d38c1498efad42759fe5f9e6d10d6bbaf982eb3e5c7efd7175e93877bcb5712d6ccd1b67178b505543b06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AsinApp::Application.config.session_store :cookie_store, key: '_asin_App_session'
